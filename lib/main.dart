5e1b58e1929ee23859a13a02093f5f6465f7aa38
Hassan Fathi Sholqamy <devhassanfathi@gmail.com>
2022-04-06 21:16:04 +0200

    added save photo and save screenshot features

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 // ignore_for_file: unnecessary_null_comparison
 
+import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:screenshot/screenshot.dart';
 
 void main() {
   runApp(const MyApp());
@@ -49,6 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
   List<DrawingArea> points = [];
   late Color selectedColor;
   late double strokeWidth;
+  final controller = ScreenshotController();
 
   @override
   void initState() {
@@ -57,13 +64,64 @@ class _MyHomePageState extends State<MyHomePage> {
     strokeWidth = 2.0;
   }
 
+  Future<String> saveImage(Uint8List bytes) async {
+    await [Permission.storage].request();
+
+    final time = DateTime.now()
+        .toIso8601String()
+        .replaceAll('.', '-')
+        .replaceAll(':', '-');
+
+    final name = 'Painting_$time';
+    final result = await ImageGallerySaver.saveImage(bytes, name: name);
+    return result['filePath'];
+  }
+
+  Widget painter() => GestureDetector(
+                      onPanDown: (details) {
+                        this.setState(() {
+                          points.add(DrawingArea(
+                              point: details.localPosition,
+                              areaPaint: Paint()
+                                ..strokeCap = StrokeCap.round
+                                ..isAntiAlias = true
+                                ..color = selectedColor
+                                ..strokeWidth = strokeWidth));
+                        });
+                      },
+                      onPanUpdate: (details) {
+                        this.setState(() {
+                          points.add(DrawingArea(
+                              point: details.localPosition,
+                              areaPaint: Paint()
+                                ..strokeCap = StrokeCap.round
+                                ..isAntiAlias = true
+                                ..color = selectedColor
+                                ..strokeWidth = strokeWidth));
+                        });
+                      },
+                      onPanEnd: (details) {
+                        this.setState(() {
+                          // ignore: dead_code
+                          points.add(DrawingArea(
+                              point: Offset.zero, areaPaint: Paint()));
+                        });
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        child: CustomPaint(
+                          painter: MyCustomPainter(points: points),
+                        ),
+                      ),
+                    );
+
   void selectColor() {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
         title: Text("Choose Your Color"),
         content: SingleChildScrollView(
-          child: BlockPicker(
+          child: ColorPicker(
             pickerColor: selectedColor,
             onColorChanged: (color) {
               this.setState(() {
@@ -88,133 +146,121 @@ class _MyHomePageState extends State<MyHomePage> {
     final double width = MediaQuery.of(context).size.width;
     final double height = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        elevation: 0,
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        title: Text(widget.title),
-      ),
-      body: Stack(
-        children: <Widget>[
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                  Color.fromARGB(255, 155, 40, 200),
-                  Color.fromARGB(255, 46, 22, 182)
-                ])),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: width * 0.8,
-                  height: height * 0.8,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.black.withOpacity(0.4),
-                            blurRadius: 5.0,
-                            spreadRadius: 1.0),
-                      ]),
-                  child: GestureDetector(
-                    onPanDown: (details) {
-                      this.setState(() {
-                        points.add(DrawingArea(
-                            point: details.localPosition,
-                            areaPaint: Paint()
-                              ..strokeCap = StrokeCap.round
-                              ..isAntiAlias = true
-                              ..color = selectedColor
-                              ..strokeWidth = strokeWidth));
-                        print("down");
-                        print(details.localPosition);
-                      });
-                    },
-                    onPanUpdate: (details) {
-                      this.setState(() {
-                        points.add(DrawingArea(
-                            point: details.localPosition,
-                            areaPaint: Paint()
-                              ..strokeCap = StrokeCap.round
-                              ..isAntiAlias = true
-                              ..color = selectedColor
-                              ..strokeWidth = strokeWidth));
-                        print("update");
-                        print(details.localPosition);
-                      });
-                    },
-                    onPanEnd: (details) {
-                      this.setState(() {
-                        // ignore: dead_code
-                        points.add(DrawingArea(
-                            point: Offset.zero, areaPaint: Paint()));
-                      });
-                    },
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      child: CustomPaint(
-                        painter: MyCustomPainter(points: points),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: width * 0.8,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      IconButton(
-                        onPressed: () {
-                          selectColor();
-                        },
-                        icon: Icon(Icons.color_lens),
-                        iconSize: 20,
-                        color: selectedColor,
-                      ),
-                      Expanded(
-                          child: Slider(
-                              min: 1.0,
-                              max: 7.0,
-                              activeColor: selectedColor,
-                              value: strokeWidth,
-                              onChanged: (value) {
-                                this.setState(() {
-                                  strokeWidth = value;
-                                });
-                              })),
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            points.clear();
-                          });
-                        },
-                        icon: Icon(Icons.layers_clear),
-                        iconSize: 20,
-                      ),
-                    ],
-                  ),
-                )
-              ],
+    return Screenshot(
+      controller: controller,
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        // appBar: AppBar(
+        //   elevation: 0,
+        //   centerTitle: true,
+        //   backgroundColor: Colors.transparent,
+        //   title: Text(widget.title),
+        // ),
+        body: Stack(
+          children: <Widget>[
+            SizedBox(
+              height: 20,
             ),
-          )
-        ],
+            Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                    Color.fromARGB(255, 155, 40, 200),
+                    Color.fromARGB(255, 46, 22, 182)
+                  ])),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: width * 0.65,
+                    height: height * 0.8,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black.withOpacity(0.4),
+                              blurRadius: 5.0,
+                              spreadRadius: 1.0),
+                        ]),
+                    child: painter()
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: width * 0.65,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        IconButton(
+                          onPressed: () {
+                            selectColor();
+                          },
+                          icon: Icon(Icons.color_lens),
+                          iconSize: 20,
+                          color: selectedColor.withOpacity(1.0),
+                        ),
+                        Expanded(
+                            child: Slider(
+                                min: 1.0,
+                                max: 7.0,
+                                activeColor: selectedColor.withOpacity(1.0),
+                                value: strokeWidth,
+                                onChanged: (value) {
+                                  this.setState(() {
+                                    strokeWidth = value;
+                                  });
+                                })),
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              points.clear();
+                            });
+                          },
+                          icon: Icon(Icons.layers_clear),
+                          iconSize: 20,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+        floatingActionButton: SpeedDial(
+          animatedIcon: AnimatedIcons.menu_close,
+          backgroundColor: Color.fromARGB(255, 155, 40, 200),
+          children: [
+            SpeedDialChild(child: Icon(Icons.upload_file)),
+            SpeedDialChild(
+                child: Icon(Icons.screenshot),
+                label: 'Save Screenshot',
+                onTap: () async {
+                  final screenshotImage = await controller.capture();
+                  if (screenshotImage == null) return;
+                  await saveImage(screenshotImage);
+                }),
+            SpeedDialChild(
+                label: 'Save Painting',
+                child: Icon(Icons.save),
+                onTap: () async {
+                  final paintingImage =
+                      await controller.captureFromWidget(painter());
+
+                  if (paintingImage == null) return;
+                  await saveImage(paintingImage);
+                })
+          ],
+        ),
       ),
     );
   }
@@ -233,18 +279,14 @@ class MyCustomPainter extends CustomPainter {
     canvas.drawRect(box, background);
     DrawingArea endPan =
         new DrawingArea(point: Offset.zero, areaPaint: Paint());
-    // Paint drawing = Paint();
-    // drawing.color = Colors.black;
-    // drawing.strokeWidth = 2;
-    // drawing.isAntiAlias = true;
-    // drawing.strokeCap = StrokeCap.round;
 
     for (int x = 0; x < points.length - 1; x++) {
-
-      if (points[x].point != Offset.zero && points[x + 1].point != Offset.zero) {
+      if (points[x].point != Offset.zero &&
+          points[x + 1].point != Offset.zero) {
         Paint drawing = points[x].areaPaint;
         canvas.drawLine(points[x].point, points[x + 1].point, drawing);
-      } else if (points[x].point != Offset.zero && points[x + 1].point == Offset.zero) {
+      } else if (points[x].point != Offset.zero &&
+          points[x + 1].point == Offset.zero) {
         Paint drawing = points[x].areaPaint;
         canvas.drawPoints(PointMode.points, [points[x].point], drawing);
       }
